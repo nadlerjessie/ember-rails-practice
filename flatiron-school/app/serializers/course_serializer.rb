0703659f39983e4513beaa9cedeb711b5d9dfb20
nadlerjessie <nadler.jessie@gmail.com>
2015-11-25 17:26:03 -0500

    added has many relationship for courses to teachers

--- flatiron-school/app/serializers/course_serializer.rb
+++ flatiron-school/app/serializers/course_serializer.rb
@@ -2,4 +2,5 @@ class CourseSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :name
   has_many :students
+  has_many :teachers
 end
--- flatiron-school/app/serializers/teacher_serializer.rb
+++ flatiron-school/app/serializers/teacher_serializer.rb
@@ -1,4 +1,4 @@
 class TeacherSerializer < ActiveModel::Serializer
-  attributes :id, :name
-  
+  attributes :id, :name, :course_id
+  has_many :students, through: :course
 end
